ec5ff3104caa2a3086bbfce87b782880bc13f0bb
Ryan Giordano <rgiorda1@gmail.com>
2016-02-21 20:23:57 -0500

    added footer

--- js/controllers/virusController.js
+++ js/controllers/virusController.js
@@ -6,7 +6,7 @@ app.controller('virusController',function($scope){
       color: "#15639D",
       symptoms: "Low fever, shivering, runny nose, sore throat",
       severity: "mild",
-      date_discovered: "",
+      date_discovered: '20140313T00:00:00',
       description:"A blue devil that chills the afflicted to the bone, sending them into uncontrollable convulsions.",
       gallery: 'blueanimate.gif',
       image:'chill.png'
@@ -26,7 +26,7 @@ app.controller('virusController',function($scope){
       color: "#FDBD30",
       symptoms: "High fever, sweats, headache, drowsiness",
       severity: "severe",
-      date_discovered: "",
+      date_discovered: '20140313T00:00:00',
       description:"A strange yellow fellow that causes depression and anxiety in the afflicted individual.",
       gallery: 'yellowanimate.gif',
       image:'weird.png'
